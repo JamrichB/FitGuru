1788fa4f0deb2f7ae0b5f9ca5e33df48196a766d
JamrichB <barbor.jamrichova@gmail.com>
2020-09-08 22:13:32 +0200

    Icons, Responsivity (20%)

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const navSlider = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_comp');
+    const navLinks = document.querySelectorAll('.nav_comp li');
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+    
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .1}s`;
+            }
+        });
+
+    burger.classList.toggle('toggle');
+    });
+}
+
+navSlider();
