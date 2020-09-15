8c571b29f11d666fa5dd1a893f76601f2b3a86b4
JamrichB <barbor.jamrichova@gmail.com>
2020-09-15 09:05:48 +0200

    Scroll animation Done

--- js/script.js
+++ js/script.js
@@ -18,4 +18,37 @@ const navSlider = () => {
     });
 }
 
+function scrollEffect() {
+    var aboutText = document.querySelector('.about');
+    var aboutPosition = aboutText.getBoundingClientRect().top;
+
+    var placesText = document.querySelector('.placesClass');
+    var placesPosition = placesText.getBoundingClientRect().top;
+
+    var compoText = document.querySelector('.components');
+    var compoPosition = compoText.getBoundingClientRect().top;
+
+    var tableText = document.querySelector('.scTable');
+    var tablePosition = tableText.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.5;
+   
+
+  if(aboutPosition < screenPosition){
+     aboutText.classList.add('about_animate');
+  }
+  if(placesPosition < screenPosition) {
+    placesText.classList.add('placesClass_animate');
+  }
+  if(compoPosition < screenPosition) {
+      compoText.classList.add('components_animate');
+  }
+  if(tablePosition < screenPosition) {
+      tableText.classList.add('scTable_animate')
+  }
+} 
+
 navSlider();
+window.addEventListener('scroll',scrollEffect);
+
+
+
